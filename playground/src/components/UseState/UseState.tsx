f579a299da2c69c7b63de38b34d9baab9e78755f
Enrique Ruvalcaba <jordison352@gmail.com>
2023-04-18 12:31:55 -0600

    independent states in functional components

--- playground/src/components/UseState/UseState.tsx
+++ playground/src/components/UseState/UseState.tsx
@@ -1,10 +1,13 @@
-import { useEffect, useState } from "react";
+import { ChangeEvent, useEffect, useState } from "react";
+
+const SECURITY_CODE = "paradigma";
 
 interface UseStatePros {
   name: string;
 }
 
 export const UseState = ({ name }: UseStatePros) => {
+  const [value, setValue] = useState("");
   const [error, setError] = useState(false);
   const [loading, setLoading] = useState(false);
 
@@ -14,6 +17,8 @@ export const UseState = ({ name }: UseStatePros) => {
     console.log("effect");
 
     setTimeout(() => {
+      if (value !== SECURITY_CODE) setError(true);
+
       setLoading(false);
     }, 2000);
   }, [loading]);
@@ -23,14 +28,22 @@ export const UseState = ({ name }: UseStatePros) => {
       <h2>Delete {name}</h2>
       <p>Please provide the security code. </p>
 
-      {error && <p>Error: the code is invalid.</p>}
+      {error && !loading && <p>Error: the code is invalid.</p>}
 
       {loading && <p>Loading....</p>}
 
-      <input type="text" name="code" id="code" />
+      <input
+        type="text"
+        name="code"
+        id="code"
+        value={value}
+        onChange={(e: ChangeEvent<HTMLInputElement>) =>
+          setValue(e.target.value)
+        }
+        onFocus={() => setError(false)}
+      />
       <button
         onClick={() => {
-          setError(!error);
           setLoading(true);
         }}
       >
